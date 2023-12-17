d82b63e601450bd511464d43f49abb8f06a9471f
Himanshu Mahtolia <114689084+himanshumahtolia@users.noreply.github.com>
2023-12-17 10:22:08 +0530

    Update Simpson3by8.cpp

--- Simpson3by8.cpp
+++ Simpson3by8.cpp
@@ -1,33 +1,31 @@
 #include<iostream>
-#include<iomanip>
-
-
+#include<math.h>
 using namespace std;
-
-float f(float x,float y){
-	
-	return x+y*y;
-}
+#define f(x) x*x
 int main(){
-	float x0,y0,h,xn,x,y,k1,k2,k3,k4,k;
-	cout<<"Enter initial value (x0,y0) :- ";
-	cin>>x0>>y0;
-	cout<<"Enter step size ,end point :- ";
-	cin>>h>>xn;
-	x=x0;
-	y=y0;
-	cout<<fixed;
-	while(x<xn){
-		k1=h*f(x,y);
-		k2=h*f(x+h/2,y+k1/2);
-		k3=h*f(x+h/2,y+k2/2);
-		k4=h*f(x+h,y+k3);
-		k=(k1+2*k2+2*k3+k4)/6;
-		y+=k;
-		x+=h;
-		cout<<"when x = "<<setprecision(4)<<setw(8)<<x;
-		cout<<endl<<" y= "<<setw(8)<<y<<endl;
-	}
+	float l,u,intg=0,step,k;
+	int subin;
+	cout<<" cout lower limit :- ";
+	cin>>l;
 	
+	cout<<" cout upper limit :- ";
+	cin>>u;
+	cout<<" enter subinterval :- ";
+	cin>>subin;
+	step=(u-l)/subin;
+	intg=f(l)+f(u);
+	for(int i=1;i<subin;i++){
+		k=(l+i)*step;
+		if(i%3==0){
+			intg+=2*(f(k));
+		}
+		else{
+			intg+=3*(f(k));
+		}
+	}
+	intg=intg*step*3.0/8.0;
+	cout<<endl <<"ans "<<intg;
+	
+
 return 0;
 }
